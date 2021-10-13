ddf59e39ee38b38ebad17c031f3641d1724866ca
Luis Andrés Arce C <luilliarcec@gmail.com>
2021-10-13 11:47:03 -0500

    WIP

--- src/EcuadorLocationsServiceProvider.php
+++ src/EcuadorLocationsServiceProvider.php
@@ -11,13 +11,13 @@ class EcuadorLocationsServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             if (!class_exists('CreateProvinciasTable')) {
                 $this->publishes([
-                    __DIR__ . '/../Database/Migrations/create_provincias_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_provincias_table.php'),
+                    __DIR__ . '/../database/migrations/create_provincias_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_provincias_table.php'),
                 ], 'migrations');
             }
 
             if (!class_exists('CreateCantonesTable')) {
                 $this->publishes([
-                    __DIR__ . '/../Database/Migrations/create_cantones_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_cantones_table.php'),
+                    __DIR__ . '/../database/migrations/create_cantones_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_cantones_table.php'),
                 ], 'migrations');
             }
         }
